deb9265374a7225667c92d0c87f5198f84e80339
besso <besso@live.com>
2012-01-27 15:20:27 -0600

    config dynamic

--- config.php
+++ config.php
@@ -1,13 +1,21 @@
 <?php
+
+$service = 'dev';
+//$service = 'backyard';
+
+$server_ip  = $_SERVER['SERVER_ADDR'];
+$server_url = 'http://' . $server_ip . '/';
+$server_path = '/var/www/html/' . $service . '/';
+
 // HTTP
-define('HTTP_SERVER2', 'http://168.93.73.186/');
-define('HTTP_SERVER', 'http://168.93.73.186/dev/');
-define('HTTP_CATALOG', 'http://168.93.73.186/dev/');
-define('HTTP_IMAGE', 'http://168.93.73.186/dev/image/');
+define('HTTP_SERVER2',$server_url);
+define('HTTP_SERVER', $server_url . $service . '/' );
+define('HTTP_CATALOG',$server_url . $service . '/');
+define('HTTP_IMAGE',  $server_url . $service . '/image/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://168.93.73.186/dev/');
-define('HTTPS_IMAGE', 'http://168.93.73.186/dev/image/');
+define('HTTPS_SERVER',$server_url . $service . '/');
+define('HTTPS_IMAGE', $server_url . $service . '/image/');
 
 /* todo. better to use defined VAR
 echo phpinfo();
@@ -15,17 +23,17 @@ echo $_SERVER["DOCUMENT_ROOT"];
 */
 
 // DIR
-define('DIR_APPLICATION', '/var/www/html/dev/');
-define('DIR_SYSTEM','/var/www/html/dev/system/');
-define('DIR_DATABASE','/var/www/html/dev/system/database/');
-define('DIR_LANGUAGE', '/var/www/html/dev/language/');
-define('DIR_TEMPLATE', '/var/www/html/dev/view/template/');
-define('DIR_CONFIG', '/var/www/html/dev/system/config/');
-define('DIR_IMAGE', '/var/www/html/dev/image/');
-define('DIR_CACHE', '/var/www/html/dev/system/cache/');
-define('DIR_DOWNLOAD', '/var/www/html/dev/download/');
-define('DIR_LOGS', '/var/www/html/dev/system/logs/');
-define('DIR_CATALOG', '/var/www/html/dev/catalog/');
+define('DIR_APPLICATION', $server_path);
+define('DIR_SYSTEM', $server_path . 'system/');
+define('DIR_DATABASE', $server_path . 'system/database/');
+define('DIR_LANGUAGE', $server_path . 'language/');
+define('DIR_TEMPLATE', $server_path . 'view/template/');
+define('DIR_CONFIG',   $server_path . 'system/config/');
+define('DIR_IMAGE', $server_path . 'image/');
+define('DIR_CACHE', $server_path . 'system/cache/');
+define('DIR_DOWNLOAD', $server_path . 'download/');
+define('DIR_LOGS', $server_path . 'system/logs/');
+define('DIR_CATALOG', $server_path . 'catalog/');
 
 // DB
 define('DB_DRIVER', 'mysql');
